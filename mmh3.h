14267452ca272049e5f0e84233c262913ba3b434
daniel <daniel@planethacker.net>
2024-10-13 12:38:14 -0700

    move include to inside of ifndef

--- mmh3.h
+++ mmh3.h
@@ -1,9 +1,9 @@
 /* mmh3.h
  */
-#include <stddef.h>
-
 #ifndef MMH3_H
 #define MMH3_H
+
+#include <stddef.h>
 
 uint32_t mmh3(const uint8_t *, const uint32_t, const uint32_t);
 uint32_t mmh3_string(const char *, const uint32_t);
