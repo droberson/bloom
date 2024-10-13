0ce1725d76e605e75b678313128be15dd13e22e2
daniel <daniel@planethacker.net>
2024-10-13 13:50:59 -0700

    start with 32/64/128 bit stuff

--- bloom.h
+++ bloom.h
@@ -5,11 +5,20 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// TODO: implement 64 bit functions?
+#if UINTPTR_MAX == 0xffffffff
+typedef uint32_t (*hash_func32_t)(const void *key, size_t len, uint32_t seed);
+#define DEFAULT_HASH_FUNC_BITS
+#else
+typedef uint64_t (*hash_func64_t)(const void *key, size_t len, uint64_t seed);
+#endif /* UINTPTR_MAX */
+
 typedef struct {
-	uint32_t size;          /* size of bloom filter */
-	uint32_t hashcount;     /* number of hashes per element */
-	uint32_t bitmap_size;   /* size of bitmap */
-	uint8_t  *bitmap;       /* bitmap of bloom filter */
+	uint32_t size;              /* size of bloom filter */
+	uint32_t hashcount;         /* number of hashes per element */
+	uint32_t bitmap_size;       /* size of bitmap */
+	uint8_t  *bitmap;           /* bitmap of bloom filter */
+//	hash_funct_t hash_function; /* hash function to use. default: mmh3 */
 } bloomfilter;
 
 bool bloom_init(bloomfilter *, const uint32_t, const float);
